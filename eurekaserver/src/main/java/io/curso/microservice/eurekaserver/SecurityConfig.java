845265104a29679b69972abc896691043f7c3912
marcelodemoura <marcelodemoura14@gmail.com>
2025-08-15 10:45:49 -0300

    implementado classe de seguranca no eureka server

--- eurekaserver/src/main/java/io/curso/microservice/eurekaserver/SecurityConfig.java
+++ eurekaserver/src/main/java/io/curso/microservice/eurekaserver/SecurityConfig.java
@@ -1,4 +1,85 @@
 package io.curso.microservice.eurekaserver;
+//
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+//
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable()) // desabilita CSRF
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().authenticated() // exige autenticação para qualquer requisição
+//                )
+//                .httpBasic(withDefaults()); // autenticação básica
+//
+//        return http.build();
+//    }
+//}
+
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf()
+//                .disable()
+//                .authorizeHttpRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic();
+//    }
+//}
+
+//package io.curso.microservice.eurekaserver;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("cursoms-eureka-user")
+                .password(passwordEncoder.encode("M1c9j812"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
